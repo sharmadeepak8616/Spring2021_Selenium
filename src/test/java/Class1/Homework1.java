9d438acfdafcb403f307cb3adb5ef49a9d918e39
Deepak Sharma <sharma.deepak8616@gmail.com>
2021-05-03 21:39:30 -0400

    homework

---
+++ src/test/java/Class1/Homework1.java
@@ -0,0 +1,19 @@
+package Class1;
+
+public class Homework1 {
+
+    /**
+     * Homework 1:
+     *
+     * Create Repo in github
+     * Invite Ahsan or I as collaborator
+     * Close repo in local
+     * Create homework1 branch
+     * Create Maven project
+     * Add selenium and testNg dependencies
+     * Download ChromeDriver
+     * Create test script to launch your favourite webpage
+     * Create merge request
+     *
+     */
+}
